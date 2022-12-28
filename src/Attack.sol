25b01a74b823fed94d1bcfa5c80e16136f04774a
Pedro <pedropeter989@gmail.com>
2022-12-28 22:59:55 +0100

    second commit

--- src/Attack.sol
+++ src/Attack.sol
@@ -1,11 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 import "../src/FloraToken.sol";
 
 contract Attack {
 
-    FloraToken floratoken;
+    FloraToken public floratoken;
+    
+    constructor() payable {
+        require(msg.value == 1 ether);
+        floratoken = new FloraToken{ value : 1 ether}();
+    }
+
+    /** */
+
+    function mint(address _user, uint256 _amount) external {
+        floratoken.mintFor(_user,_amount);
+
+    }
+
+    function delegate(address _secondAddress) external {
+        floratoken.delegate(_secondAddress);
+    }
+
+    function transferOwnership(address _user) external {
+        floratoken.transferOwnership(_user);
+    }
+
 }
 
  
