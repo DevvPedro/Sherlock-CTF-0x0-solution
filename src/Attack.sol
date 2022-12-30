eb0fbfa0a2ae2f25f8befe3f3e12d8868400c500
Pedro <pedropeter989@gmail.com>
2022-12-30 10:00:10 +0100

    third commit

--- src/Attack.sol
+++ src/Attack.sol
@@ -12,21 +12,23 @@ contract Attack {
         floratoken = new FloraToken{ value : 1 ether}();
     }
 
-    /** */
+    /**@dev This function can only be called by the owner of this contract */
 
     function mint(address _user, uint256 _amount) external {
         floratoken.mintFor(_user,_amount);
 
     }
-
+    
+    /**@dev must be called by the address (_user) */
     function delegate(address _secondAddress) external {
         floratoken.delegate(_secondAddress);
     }
 
+    /**@dev must  be called by address(_secondAddress)*/
+
     function transferOwnership(address _user) external {
         floratoken.transferOwnership(_user);
     }
-
 }
 
  
